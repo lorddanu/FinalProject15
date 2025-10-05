c71dee226715781c15102d6b037b094650e47d54
zhafranabimata <adyaganteng981@gmail.com>
2025-10-05 18:31:51 +0700

    baris ketiga

--- test.java
+++ test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hi, saya danu ini baris kode pertama saya");
         System.out.println("Hi, saya danu ini baris kode kedua saya");
+        System.out.println("Hi, saya danu ini baris kode ketiga saya");
+    
     }
 }
