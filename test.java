d94e50e8cc11fa401c7089ffa02e72021bf3bdad
zhafranabimata <adyaganteng981@gmail.com>
2025-10-05 18:31:24 +0700

    iya

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hi, saya danu ini baris kode pertama saya");
+        System.out.println("Hi, saya danu ini baris kode kedua saya");
     }
 }
